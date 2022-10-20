c830c8b642cdcf4d3e72f27cdfd4daa9c1e0d28c
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2022-10-20 17:51:14 -0400

    Linked Heroku with GitHub for uniform version control

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const mongoose = require('mongoose'),
     Genres = Models.Genre,
     Directors = Models.Director;
 
+    
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
