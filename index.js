f2c3aa9d0982f1f4d0f18d84f5a25e1d0052dd50
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2022-10-21 11:23:28 -0400

    Repeat Commit - Environmental Variables

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ const mongoose = require('mongoose'),
     Users = Models.User,
     Genres = Models.Genre,
     Directors = Models.Director;
-
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+    
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 app.use(morgan('combined', {stream: accessLogStream}));
