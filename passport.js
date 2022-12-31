0bc08f4705f26685c1cef7fb00d3c6de084b28bf
justindegaetano <105392908+justindegaetano@users.noreply.github.com>
2022-12-31 17:51:15 -0500

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,31 +1,34 @@
 const passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy,
-  Models = require('./models.js'),
-  passportJWT = require('passport-jwt');
+//defines basic HTTP authentication for login request
+LocalStrategy = require('passport-local').Strategy,
+Models = require('./models.js'),
+passportJWT = require('passport-jwt');
 
 let Users = Models.User,
-  JWTStrategy = passportJWT.Strategy,
-  ExtractJWT = passportJWT.ExtractJwt;
+JWTStrategy = passportJWT.Strategy,
+ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
+  //LocalStrategy takes a username and password from the request body and uses mongoose to check the database for a user with the same username but the password doesn't get checked here.
   usernameField: 'Username',
   passwordField: 'Password'
 }, (username, password, callback) => {
-  console.log(username + '  ' + password);
+  console.log(username + ' ' + password);
   Users.findOne({ Username: username }, (error, user) => {
-    if (error) {
+    if(error) {
       console.log(error);
       return callback(error);
     }
-
-    if (!user) {
+    // if the username cannot be found
+    if(!user) {
       console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password.'});
+      return callback(null, false, {message: 'Incorrect username.'});
     }
-    
+   
+    // validatation of user's password
     if (!user.validatePassword(password)) {
-        console.log('incorrect password');
-        return callback(null, false, {message: 'Incorrect password.'});
+      console.log('incorrect password');
+      return callback(null, false, {message: 'Incorrect password.'});
     }
 
     console.log('finished');
@@ -33,15 +36,16 @@ passport.use(new LocalStrategy({
   });
 }));
 
-passport.use(new JWTStrategy({
+//jwt is extracted from the header of the http request. jwt is called bearer token
+passport.use(new JWTStrategy ({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: 'your_jwt_secret'
 }, (jwtPayload, callback) => {
   return Users.findById(jwtPayload._id)
-    .then((user) => {
-      return callback(null, user);
-    })
-    .catch((error) => {
-      return callback(error)
-    });
+  .then((user) => {
+    return callback(null, user);
+  })
+  .catch((error) => {
+    return callback(error)
+  });
 }));
